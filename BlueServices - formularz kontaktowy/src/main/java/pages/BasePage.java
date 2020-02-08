ba211c3954770dcc0ef20380fc6ff43a0d15664d
akieliszczyk <38585186+akieliszczyk@users.noreply.github.com>
2020-02-08 23:49:39 +0100

    Refactor BasePage.class

--- BlueServices - formularz kontaktowy/src/main/java/pages/BasePage.java
+++ BlueServices - formularz kontaktowy/src/main/java/pages/BasePage.java
@@ -3,11 +3,12 @@ package pages;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
-public abstract class BasePage <driver> {
+public abstract class BasePage {
 
     protected WebDriver driver;
 
     public BasePage(WebDriver driver) {
+        this.driver=driver;
         PageFactory.initElements(driver, this);
     }
 }
